1c576b9082211ae1ebb40cd0215e6fb5c6b8c24c
adityasunil09 <adityasunil54m5@gmail.com>
2024-11-07 17:35:29 +0530

    Calculator

---
+++ src/components/Input.js
@@ -0,0 +1,16 @@
+import "./Input.css"
+
+const Input = ({ text, result}) => {
+    return(
+         <div className="input-wrapper">
+    <div className="result">
+        <h1>{result}</h1>
+    </div>
+
+    <div className="text"></div>
+    <h3>{text}</h3>
+    </div>
+    );
+};
+
+export default Input;
